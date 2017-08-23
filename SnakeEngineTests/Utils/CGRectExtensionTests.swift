eca0a4ec23a148c147bc1c17cd27b9202a87e77b
Chris Jimenez <cjimenez16@gmail.com>
2017-08-23 23:21:58 +0200

    Update unit tests for CGRect

---
+++ SnakeEngineTests/Utils/CGRectExtensionTests.swift
@@ -0,0 +1,32 @@
+//
+//  CGPointExtensionsTests.swift
+//  Snake
+//
+//  Created by Chris Jimenez on 8/13/17.
+//  Copyright © 2017 Chris Jimenez. All rights reserved.
+//
+import XCTest
+import Foundation
+@testable import SnakeEngine
+
+class CGRectExtensionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testRandomPointGeneratedInsideRect() {
+        
+        let rect = CGRect(x: -500, y: -700, width: 500, height: 700)
+        
+        let point =  rect.randomPointInRectWithMargin(margin: 50)
+        
+        XCTAssertTrue(rect.contains(point))
+    }
+    
+}
