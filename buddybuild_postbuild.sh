0d389ffebfaf1e17ea4a2bd3ac1c3308d2febf17
Chris Jimenez <cjimenez16@gmail.com>
2017-08-26 11:37:03 +0200

    Update code coderage report

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/env bash
-
-curl -s https://codecov.io/bash -t 4c719f72-7355-4bc5-99f7-212bd3b77985
+#codecov
+bash <(curl -s https://codecov.io/bash) -t 4c719f72-7355-4bc5-99f7-212bd3b77985 -J 'Snake'
