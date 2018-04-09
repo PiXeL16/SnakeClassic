e553180b96860ecb642ea1d9a359fe445036aab1
Akshay Sud <sudakshay@gmail.com>
2018-04-10 11:24:24 +1200

    Break the build

--- Snake/AppDelegate.swift
+++ Snake/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        //return monkey 
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
